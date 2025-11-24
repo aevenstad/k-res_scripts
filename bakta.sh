0ca3c2a68f6007793a0570c0f3002d93b0f69f3e
aevenstad <andreasevenstad@gmail.com>
2025-11-24 15:40:24 +0100

    add species argument to bakta & rename prefix argument

--- bakta.sh
+++ bakta.sh
@@ -9,11 +9,11 @@ bakta="quay.io-biocontainers-bakta-1.10.4--pyhdfd78af_0.img"
 
 # Pull container if it doesn't exist on the system
 if [ ! -f "$container_dir/$bakta" ]; then
-    echo "Container not found. Pulling from registry..."
-    singularity pull --name "$container_dir/$bakta" "docker://quay.io/biocontainers/bakta:1.10.4--pyhdfd78af_0"
-    if [ $? -ne 0 ]; then
-        exit 1
-    fi
+  echo "Container not found. Pulling from registry..."
+  singularity pull --name "$container_dir/$bakta" "docker://quay.io/biocontainers/bakta:1.10.4--pyhdfd78af_0"
+  if [ $? -ne 0 ]; then
+    exit 1
+  fi
 fi
 
 # Bakta database
@@ -21,60 +21,60 @@ kres_192=/bigdata/Jessin/Softwares/Database/bakta_db/db
 kres_130=/bigdata/Jessin/Softwares/bakta_db/db
 kres_128=/bigdata/Jessin/Softwares/bakta/db/
 
-
 if [ -d "$kres_192" ]; then
-    bakta_db="$kres_192"
+  bakta_db="$kres_192"
 elif [ -d "$kres_130" ]; then
-    bakta_db="$kres_130"
+  bakta_db="$kres_130"
 elif [ -d "$kres_128" ]; then
-    bakta_db="$kres_128"
+  bakta_db="$kres_128"
 else
-    echo "No valid BAKTA database found. Please check the database path."
-    exit 1
+  echo "No valid BAKTA database found. Please check the database path."
+  exit 1
 fi
 
-
 Help() {
-    echo "Usage: $0 [options]"
-    echo "Options:"
-    echo "  -h                Show this help message"
-    echo "  -s <sample_name>  Specify sample name (output prefix)"
-    echo "  -i <file>         Specify assembly file (FASTA format)"
+  echo "Usage: $0 [options]"
+  echo "Options:"
+  echo "  -h                Show this help message"
+  echo "  -p <prefix>       Specify sample name (output prefix)"
+  echo "  -i <file>         Specify assembly file (FASTA format)"
+  echo "  -s <species>      Species name"
 }
 
 if [[ " $@ " =~ " -h " ]]; then
-    Help
-    exit 0
+  Help
+  exit 0
 fi
 
-while getopts ":h:s:i:o:" option; do
-    case $option in
-        h)
-            Help
-            exit 0
-            ;;
-        s)  
-            sample="$OPTARG"
-            ;;
-        i)  
-            assembly_file="$OPTARG"
-            ;;
-        \?) 
-            echo "Invalid option: -$OPTARG" >&2
-            exit 1
-            ;;
-    esac
+while getopts ":h:p:i:o:s:" option; do
+  case $option in
+  h)
+    Help
+    exit 0
+    ;;
+  p)
+    sample="$OPTARG"
+    ;;
+  i)
+    assembly_file="$OPTARG"
+    ;;
+  s)
+    species="$OPTARG"
+    ;;
+  \?)
+    echo "Invalid option: -$OPTARG" >&2
+    exit 1
+    ;;
+  esac
 done
-
 
 # Get absolute path for assembly file
 if [ -n "$assembly_file" ]; then
-    assembly_file=$(readlink -f "$assembly_file")
+  assembly_file=$(readlink -f "$assembly_file")
 else
-    echo "Error: Assembly file is required." >&2
-    exit 1
+  echo "Error: Assembly file is required." >&2
+  exit 1
 fi
-
 
 # Prepare bind arguments for singularity
 bind_args=()
@@ -84,10 +84,10 @@ bind_args+=("-B" "$bakta_db:/bakta_db")
 # Prepare Bakta command
 output_dir=${sample}_bakta_out
 threads=12
-bakta_cmd=(bakta /data/assembly.fasta --output "${output_dir}" --prefix "${sample}" --db /bakta_db --keep-contig-headers --threads "$threads")
+bakta_cmd=(bakta /data/assembly.fasta --output "${output_dir}" --prefix "${sample}" --db /bakta_db --keep-contig-headers --threads "$threads" --species "$species")
 
 # Run BAKTA container
 singularity exec \
-    "${bind_args[@]}" \
-    "$container_dir/$bakta" \
-    "${bakta_cmd[@]}"
+  "${bind_args[@]}" \
+  "$container_dir/$bakta" \
+  "${bakta_cmd[@]}"
